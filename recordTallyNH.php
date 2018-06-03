82b7d04e1d3e895641d73b1f353141127f0aa3ff
Mica Cardillo <mica.cardillo@gmail.com>
2018-06-02 20:26:41 -0700

    finally

--- recordTallyNH.php
+++ recordTallyNH.php
@@ -47,8 +47,6 @@
 		$monthCode = $defaultPickupDateValues['MONTH_CODE'];
 		$monthShort = $defaultPickupDateValues['MONTH_SHORT'];
 
-		echo "<h1>'.$monthCode.'</h1>";
-
 		echo '<h1 style="color:green; padding-bottom: 15px;"> Record Tally for '.$nhName.' </h1> ';
 
 		echo '<div id="tallyFormDiv">';
@@ -65,6 +63,7 @@
 			<span style="font-size: 18px; color: red; padding: 5px; font-weight: bolder;"> Choose Nearest Month & Year </span>
 
 				<select style="font-size: 16px; margin-left: 15px; color: red; font-weight: bolder; border: 1px solid #bbb1a7; border-radius: 5px;" name="pickupMonth">
+					<option style="background-color:yellow;" value="'.$monthCode.'"}>'.$monthShort.'</option>
 					<option value="02">Feb </option>
 					<option value="04">Apr</option>
 					<option value="06">Jun </option>
